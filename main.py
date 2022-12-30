2a63b15bc766ec92f96da12d196234dde1752d52
schneiderautomatyka <schneiderautomatyka@gmail.com>
2022-12-30 22:22:04 +0100

    Thread inheritance
    + Thread inheritance
    + Better Stop function
    + Assertions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import s7broker
 import pandas as pd
 import time
-from threading import Thread, Event
+from threading import Thread
 from queue import Queue, Empty
+
 
 def consumer_thread(thread_timeout_s:float, plc_queue:Queue):
     
@@ -18,10 +19,9 @@ def consumer_thread(thread_timeout_s:float, plc_queue:Queue):
         except Empty: 
             off_condition = True
         except AttributeError:
-            print(f'PLC data might have a wrong structure')
+            print(f'PLC data might have wrong structure')
     else:
         print('Consumer thread ended')
-
 
 
 PLC_IP='192.168.33.6'
@@ -29,27 +29,24 @@ DB_NUMBER = 1
 INTERVAL_S = 1
 CONSUMER_TIMEOUT_S = 10
 
-plc_queue = Queue()
-stop_event = Event()
 
 # Create a broker and use necessary functions
 broker = s7broker.Broker('ExchangeData.xlsx')
-broker.prepare_value_frame()
-broker.compute_additional_offset()
-broker.define_full_byte_range()
+print(broker)
+broker.auto_config()
+broker.change_connection_options(PLC_IP, DB_NUMBER, INTERVAL_S)
 
 
-broker_thread = Thread(target=broker.broker_thread, args=(PLC_IP, DB_NUMBER, INTERVAL_S, plc_queue, stop_event))
-plc_consumer_thread = Thread(target=consumer_thread, args=(CONSUMER_TIMEOUT_S, plc_queue))
+plc_consumer_thread = Thread(target=consumer_thread, args=(CONSUMER_TIMEOUT_S, broker.broker_queue))
 
-broker_thread.start()
+broker.start()
 plc_consumer_thread.start()
 
 try:
-    while broker_thread.is_alive() and plc_consumer_thread.is_alive():
+    while broker.is_alive() and plc_consumer_thread.is_alive():
         time.sleep(1)
 except KeyboardInterrupt:
-    stop_event.set()
-    broker_thread.join()
+    broker.stop()
+    broker.join()
     plc_consumer_thread.join()
     
--- s7broker.py
+++ s7broker.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 import snap7
 import time
+import socket
 from queue import Queue
-from threading import Event
+from threading import Event, Thread
 
 
 s7_bytes_to_read = {
@@ -18,6 +19,7 @@ s7_bits_to_read = {
     'Bool' : 1,    
 }
 
+# ONLY BOOL TESTED
 s7_additional_offset = {
     'Int'  : 2,
     'Real' : 4,
@@ -85,7 +87,7 @@ def s7frame_extract(s7frame:bytearray, offset:float, data_type:str):
     return value
 
 
-class Broker():
+class Broker(Thread):
 
     '''
     Broker class
@@ -95,43 +97,120 @@ class Broker():
         - config_file_path (the file contains specified TIA portal db interface)
     '''
 
-    def __init__(self, config_file_path:str):
+    def __init__(self, config_file_path:str, *args, **kwargs):
         '''
         Save config file path
         Initialize s7 client
         '''
+        super().__init__(*args, **kwargs)
         self.config_file_path = config_file_path
         self.plc_client = snap7.client.Client()
-        self.additional_offset = 0
-        self.offset_start = 0
-        self.offset_stop = 0
-    
+        self.broker_queue = Queue()
+        self.broker_stop_event = Event()
+        self.additional_offset = None
+        self.offset_start = None
+        self.offset_stop = None
+        self.df_values_created = None
+        self.plc_ip = None
+        self.datablock_number = None
+        self.interval_s = None 
+        
+    def __str__(self):
+        info = '''
+        This is the Broker class
+        It requires a proper plc's db .xlsx config file within the config_file_path
+        Before starting a broker, additional functions must be invoked
+        Check -> auto_config()
+        '''
+        return info
+        
     def prepare_value_frame(self):
         '''
-        Read config file, add an empty 'Value' column
+        Read config file, create new value dataframe
         '''
         self.df_datablock_plc = pd.read_excel('ExchangeData.xlsx', usecols=['Name', 'Data type', 'Offset', 'Comment'])
         self.df_datablock_plc['Value'] = None
         self.df_values = self.df_datablock_plc[['Offset', 'Value', 'Data type', 'Name']].copy().set_index('Offset')
+        self.df_values_created = True
+        return 'Broker> Value dataframe successfully created'
     
     def compute_additional_offset(self):
         '''
         Depending on the type of the last value in datablock the max byte range is likely to change
         adjusting additional offset prevents the problem
         '''
+        assert self.df_values_created == True
         last_offset = self.df_values.iloc[-1]
         last_value_type = last_offset['Data type']
-        self.additional_offset = s7_additional_offset[last_value_type]  
+        self.additional_offset = s7_additional_offset[last_value_type]
+        return 'Broker> Additional offset added'  
 
     def define_full_byte_range(self):
         '''
         Define byte range to read, read it all, offset is the index
         '''
+        assert not self.additional_offset is None
         self.offset_start = int(self.df_values.index.min())
         self.offset_stop = int(np.ceil(self.df_values.index.max())) + self.additional_offset
-
+        return 'Broker> Full byte range set'
+        
+    def auto_config(self):
+        '''
+        Perform auto configuration of essential class parameters
+        Manually the functions bellow must be invoked (in the right order):
+            - prepare_value_frame()
+            - compute_additional_offset()
+            - define_full_byte_range()
+        '''
+        print(self.prepare_value_frame())
+        print(self.compute_additional_offset())
+        print(self.define_full_byte_range())
     
-    def reconnect_PLC(self, plc_ip:str):
+    def change_connection_options(self, plc_ip:str, datablock_number:int, interval_s:float):
+        self.plc_ip = plc_ip
+        self.datablock_number = datablock_number
+        self.interval_s = interval_s   
+        
+    def verify_configuration(self):
+        assert self.df_values_created == True
+        assert not self.offset_start is None
+        assert not self.offset_stop is None
+        assert not self.additional_offset is None
+        assert not self.plc_ip is None
+        assert not self.datablock_number is None
+        assert not self.interval_s is None
+        
+    def stop(self):
+        '''
+        Stop the broker
+        '''
+        self.broker_stop_event.set()
+    
+    def connect_PLC(self):
+        '''
+        Perform initial connection
+        '''
+        status_connected = False  
+        try:
+            socket.inet_aton(self.plc_ip)
+            self.verify_configuration()
+            self.plc_client.connect(self.plc_ip, rack=0, slot=1, tcpport=102)
+        except RuntimeError: 
+            self.broker_queue.put_nowait('kill consumer')
+            print('Broker> Could not perform initial connection, exitting ...')
+        except OSError:
+            self.broker_queue.put_nowait('kill consumer')
+            print('Broker> Wrong ip address, exitting ...')
+        except AssertionError:
+            self.broker_queue.put_nowait('kill consumer')
+            print('Broker> Wrong configuration, exitting ...')
+            
+        else:
+            print('Broker> Connected')
+            status_connected = True  
+        return status_connected
+    
+    def reconnect_PLC(self):
         '''
         Function performs max 3 attempts
         Return True if reconnected
@@ -140,38 +219,32 @@ class Broker():
         while attempt_count <= 3 and not self.plc_client.get_connected():
             print(f'Broker> Reconnecting ... attempt:{attempt_count}')
             try:
-                self.plc_client.connect(plc_ip, rack=0, slot=1, tcpport=102)
+                self.plc_client.connect(self.plc_ip, rack=0, slot=1, tcpport=102)
             except RuntimeError:
                 attempt_count += 1
         else: return self.plc_client.get_connected()
+         
             
-            
-    def broker_thread(self, plc_ip:str, datablock_number:int, interval_s:float, plc_queue:Queue, break_event:Event):
+    def run(self):
         '''
             Read plc data until the connection is interrupted,
             dataframe with values filled sent to queue is the result
         '''
-        broker_condition_stop = False   
-        try:
-            self.plc_client.connect(plc_ip, rack=0, slot=1, tcpport=102)
-        except RuntimeError: 
-            plc_queue.put_nowait('kill consumer')
-            print('Broker> Could not perform initial connection')
-        
-        broker_condition_stop = True if not self.plc_client.get_connected() else print('Broker> Connected')
-        while not broker_condition_stop and not break_event.is_set():
+        broker_condition_stop = not self.connect_PLC() 
+
+        while not broker_condition_stop and not self.broker_stop_event.is_set():
             try:
                 data_plc = self.plc_client.read_area(
                                                     area=snap7.types.Areas.DB,
-                                                    dbnumber=datablock_number,
+                                                    dbnumber=self.datablock_number,
                                                     start=self.offset_start,
                                                     size=self.offset_stop
                                                     )                        
             except RuntimeError:
                 print('Broker> Cant receive data!')
                 # Try to reconnect
-                plc_queue.put_nowait('Broker attempts to reconnect')
-                broker_condition_stop = not self.reconnect_PLC(plc_ip)
+                self.broker_queue.put_nowait('Broker attempts to reconnect')
+                broker_condition_stop = not self.reconnect_PLC()
                 
             else:
                 for offset in self.df_values.index:
@@ -179,13 +252,13 @@ class Broker():
                     value = s7frame_extract(data_plc, offset, data_type)
                     self.df_values['Value'].loc[offset] = value
                 result = self.df_values[['Value','Name']].copy().set_index('Name')
-                plc_queue.put_nowait(result)
+                self.broker_queue.put_nowait(result)
                 
             finally:
-                time.sleep(interval_s)            
+                time.sleep(self.interval_s)            
         else:
             self.plc_client.disconnect()
-            plc_queue.put_nowait('kill consumer')
+            self.broker_queue.put_nowait('kill consumer')
             print('Broker thread is finshed!')
             
 
