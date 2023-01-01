8c210b2febae2b21fcc35c314dadbac7802a0f68
schneiderautomatyka <schneiderautomatyka@gmail.com>
2023-01-01 15:42:03 +0100

    changes in s7Broker

--- BrokerSim_example.py
+++ BrokerSim_example.py
@@ -13,7 +13,6 @@ def consumer_thread(thread_timeout_s:float, plc_queue:Queue):
             plc_data = plc_queue.get(timeout=thread_timeout_s)
             if type(plc_data) is pd.DataFrame:
                 print(f'Tank 1 level:{plc_data.iloc[0].Value}')
-                print(plc_data)
             elif plc_data == 'kill consumer':
                 off_condition = True
         except Empty: 
@@ -24,12 +23,10 @@ def consumer_thread(thread_timeout_s:float, plc_queue:Queue):
         print('Consumer thread ended')
 
 
-
 CONSUMER_TIMEOUT_S = 10
 
 broker = s7broker.BrokerSim('logs/plc_data.txt','ExchangeData.xlsx')
 broker.auto_config()
-
 
 plc_consumer_thread = Thread(target=consumer_thread, args=(CONSUMER_TIMEOUT_S, broker.broker_queue))
 
